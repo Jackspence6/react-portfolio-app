403d9ac0b6cfb26defe2ace9dbc6696e17ddff2c
Jack Spence <jackspence123456@gmail.com>
2023-12-13 18:00:01 +0200

    Added initial homepage setup.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,35 @@
 // Imports
 import React from "react";
+// Icons for Homepage
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Icon from "@mui/material/Icon";
 
 // Exports
 export default function Home() {
-    return (
-        <h1>Hello World!</h1>
-    )
+	return (
+		<div>
+			<div>
+				<ul>
+					<li>
+						<a href="https://www.linkedin.com/in/jack-spence-1567b523b/">
+							<Icon component={LinkedInIcon} color="action"></Icon>
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/Jackspence6">
+							<Icon component={GitHubIcon} color="action"></Icon>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<h1>Jack</h1>
+				<h2>Spence</h2>
+				<h3>Full Stack Developer</h3>
+				<p></p>
+			</div>
+			<div></div>
+		</div>
+	);
 }
