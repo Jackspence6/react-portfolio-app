e831d41a6af0daf53839766c82f0d493cb2ab088
Jack Spence <jackspence123456@gmail.com>
2023-12-13 20:13:33 +0200

    Added typing animation for about me.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,26 @@
 // Imports
 import React from "react";
+import "../../styles/Home.css";
 // Icons for Homepage
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+// Typing animation
+import { TypeAnimation } from "react-type-animation";
 
 // Exports
 export default function Home() {
 	return (
-		<div>
-			<div>
-				<ul>
+		<div id="homeContainer">
+			<div id="leftDiv">
+				<ul id="socialLinks">
 					<li>
 						<a href="https://www.linkedin.com/in/jack-spence-1567b523b/">
 							<FontAwesomeIcon
 								icon={faLinkedin}
 								size="xl"
-								style={{ color: "#050505" }}
+								style={{ color: "#FFFFFF" }}
+								className="socialIcon"
 							/>
 						</a>
 					</li>
@@ -25,19 +29,36 @@ export default function Home() {
 							<FontAwesomeIcon
 								icon={faGithub}
 								size="xl"
-								style={{ color: "#050505" }}
-							/>{" "}
+								style={{ color: "#FFFFFF" }}
+								className="socialIcon"
+							/>
 						</a>
 					</li>
 				</ul>
 			</div>
-			<div>
-				<h1>Jack</h1>
-				<h2>Spence</h2>
-				<h3>Full Stack Developer</h3>
-				<p></p>
+			<div id="middleDiv">
+				<h1 id="name">Jack</h1>
+				<h2 id="surname">Spence</h2>
+				<h3 id="role">Full Stack Developer</h3>
+				<hr />
+				<p>
+					<TypeAnimation
+						sequence={[
+							"Hi, my name is Jack Spence - A Full-Stack Engineer with a Vision for the Future! Having successfully completed a rigorous Full-Stack Engineering boot camp, I am now poised to dive deeper into the realms of technology that fascinate me the most - generative AI and machine learning. My journey in software development, enriched by my background in marketing, has provided me with a unique perspective on how technology can be leveraged to not just meet but anticipate user needs. My experience has endowed me with a robust foundation in various programming languages and frameworks. I am now keen on focusing my skills towards the cutting-edge fields of generative AI and machine learning. I am particularly interested in exploring how these technologies can be used to create innovative solutions and push the boundaries of what's possible in tech. Looking ahead, my goal is to be at the forefront of the AI revolution, contributing to projects that utilize AI and ML to solve complex problems, enhance user experiences, and open up new possibilities. I aspire to be part of teams and projects that are not just technology-driven but also vision-oriented, creating a future where technology acts as a catalyst for positive change and unprecedented innovation.",
+						]}
+						wrapper="p"
+						speed={75}
+						style={{
+							fontSize: "15px",
+							display: "inline-block",
+							fontFamily: "monospace",
+						}}
+						repeat={0}
+						deletionSpeed={90}
+					/>
+				</p>
 			</div>
-			<div></div>
+			<div id="rightDiv"></div>
 		</div>
 	);
 }
