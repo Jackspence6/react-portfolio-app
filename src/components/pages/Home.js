7a0d4403690299beb7d9ca2c0fecc2ada73063bc
Jack Spence <jackspence123456@gmail.com>
2023-12-14 14:44:36 +0200

    Improved styling for about me on home page.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -57,11 +57,14 @@ export default function Home() {
 								"Hi, my name is Jack Spence - A Full-Stack Engineer with a Vision for the Future! Having successfully completed a rigorous Full-Stack Engineering boot camp, I am now poised to dive deeper into the realms of technology that fascinate me the most - generative AI and machine learning. My journey in software development, enriched by my background in marketing, has provided me with a unique perspective on how technology can be leveraged to not just meet but anticipate user needs. My experience has endowed me with a robust foundation in various programming languages and frameworks. I am now keen on focusing my skills towards the cutting-edge fields of generative AI and machine learning. I am particularly interested in exploring how these technologies can be used to create innovative solutions and push the boundaries of what's possible in tech. Looking ahead, my goal is to be at the forefront of the AI revolution, contributing to projects that utilize AI and ML to solve complex problems, enhance user experiences, and open up new possibilities. I aspire to be part of teams and projects that are not just technology-driven but also vision-oriented, creating a future where technology acts as a catalyst for positive change and unprecedented innovation.",
 							]}
 							wrapper="p"
-							speed={75}
+							speed={90}
 							style={{
-								fontSize: "15px",
+								fontSize: "20px",
 								display: "inline-block",
-								fontFamily: "monospace",
+								fontFamily: "Fira Code",
+								color: "chartreuse",
+								fontWeight: 500,
+								fontStyle: "italic",
 							}}
 							repeat={0}
 							deletionSpeed={90}
