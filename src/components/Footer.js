79dedd4d780f86da524c904929e68ba5ec37d1e3
Jack Spence <jackspence123456@gmail.com>
2023-12-14 14:27:13 +0200

    Added Footer component to page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,67 @@
 // Imports
+import "../styles/Footer.css";
+// Icons for Footer
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 // Function to render Footer
-function Footer() {}
+function Footer() {
+	return (
+		<footer className="page-footer">
+			<div className="footer-container">
+				<div className="row">
+					<div className="col l6 s12">
+						<p id="footerAbout" className="grey-text text-lighten-4">
+							Welcome to my personal portfolio, showcasing my skills and
+							projects as a full-stack developer. Built with React, it
+							highlights my expertise in creating dynamic applications,
+							featuring a sleek design and efficient navigation.
+						</p>
+					</div>
+					<div className="col l4 offset-l2 s12">
+						<h5 id="getInTouch" className="white-text">
+							Get In Touch!
+						</h5>
+						<ul className="right-align">
+							<li style={{ listStyle: "none" }}>
+								<a
+									className="grey-text text-lighten-3"
+									href="mailto:jackspence.dev@gmail.com"
+								>
+									<FontAwesomeIcon
+										icon={faEnvelope}
+										size="xl"
+										style={{ color: "#FFFFFF" }}
+										className="footerIcon"
+									/>
+								</a>
+							</li>
+							<li style={{ listStyle: "none", height: "3rem" }}>
+								<a
+									className="grey-text text-lighten-3"
+									href="https://github.com/Jackspence6"
+								>
+									<FontAwesomeIcon
+										icon={faGithub}
+										size="xl"
+										style={{ color: "#FFFFFF" }}
+										className="footerIcon"
+									/>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="footer-copyright">
+				<div>
+					<p>&copy; 2023 Jack Spence. All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+	);
+}
 
 // Exports
 export default Footer;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,147 +7,152 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 // Typing animation
 import { TypeAnimation } from "react-type-animation";
 import "../../styles/Home.css";
+import Footer from "../Footer";
 
 // Exports
 export default function Home() {
 	return (
-		<div id="homeContainer">
-			<div id="leftDiv">
-				<ul id="socialLinks">
-					<li>
-						<a
-							href="https://www.linkedin.com/in/jack-spence-1567b523b/"
-							rel="noreferrer"
-							target="_blank"
-						>
-							<FontAwesomeIcon
-								icon={faLinkedin}
-								size="xl"
-								style={{ color: "#FFFFFF" }}
-								className="socialIcon"
-							/>
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/Jackspence6"
-							rel="noreferrer"
-							target="_blank"
-						>
-							<FontAwesomeIcon
-								icon={faGithub}
-								size="xl"
-								style={{ color: "#FFFFFF" }}
-								className="socialIcon"
-							/>
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div id="middleDiv">
-				<div id="aboutContainer">
-					<h1 id="name">Jack</h1>
-					<h2 id="surname">Spence</h2>
-					<h3 id="role">Full Stack Developer</h3>
+		<div>
+			<div id="homeContainer">
+				<div id="leftDiv">
+					<ul id="socialLinks">
+						<li>
+							<a
+								href="https://www.linkedin.com/in/jack-spence-1567b523b/"
+								rel="noreferrer"
+								target="_blank"
+							>
+								<FontAwesomeIcon
+									icon={faLinkedin}
+									size="xl"
+									style={{ color: "#FFFFFF" }}
+									className="socialIcon"
+								/>
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://github.com/Jackspence6"
+								rel="noreferrer"
+								target="_blank"
+							>
+								<FontAwesomeIcon
+									icon={faGithub}
+									size="xl"
+									style={{ color: "#FFFFFF" }}
+									className="socialIcon"
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div id="middleDiv">
+					<div id="aboutContainer">
+						<h1 id="name">Jack</h1>
+						<h2 id="surname">Spence</h2>
+						<h3 id="role">Full Stack Developer</h3>
+						<hr />
+						<TypeAnimation
+							sequence={[
+								"Hi, my name is Jack Spence - A Full-Stack Engineer with a Vision for the Future! Having successfully completed a rigorous Full-Stack Engineering boot camp, I am now poised to dive deeper into the realms of technology that fascinate me the most - generative AI and machine learning. My journey in software development, enriched by my background in marketing, has provided me with a unique perspective on how technology can be leveraged to not just meet but anticipate user needs. My experience has endowed me with a robust foundation in various programming languages and frameworks. I am now keen on focusing my skills towards the cutting-edge fields of generative AI and machine learning. I am particularly interested in exploring how these technologies can be used to create innovative solutions and push the boundaries of what's possible in tech. Looking ahead, my goal is to be at the forefront of the AI revolution, contributing to projects that utilize AI and ML to solve complex problems, enhance user experiences, and open up new possibilities. I aspire to be part of teams and projects that are not just technology-driven but also vision-oriented, creating a future where technology acts as a catalyst for positive change and unprecedented innovation.",
+							]}
+							wrapper="p"
+							speed={75}
+							style={{
+								fontSize: "15px",
+								display: "inline-block",
+								fontFamily: "monospace",
+							}}
+							repeat={0}
+							deletionSpeed={90}
+						/>
+					</div>
+					<h3 id="workHeading">My Work</h3>
 					<hr />
-					<TypeAnimation
-						sequence={[
-							"Hi, my name is Jack Spence - A Full-Stack Engineer with a Vision for the Future! Having successfully completed a rigorous Full-Stack Engineering boot camp, I am now poised to dive deeper into the realms of technology that fascinate me the most - generative AI and machine learning. My journey in software development, enriched by my background in marketing, has provided me with a unique perspective on how technology can be leveraged to not just meet but anticipate user needs. My experience has endowed me with a robust foundation in various programming languages and frameworks. I am now keen on focusing my skills towards the cutting-edge fields of generative AI and machine learning. I am particularly interested in exploring how these technologies can be used to create innovative solutions and push the boundaries of what's possible in tech. Looking ahead, my goal is to be at the forefront of the AI revolution, contributing to projects that utilize AI and ML to solve complex problems, enhance user experiences, and open up new possibilities. I aspire to be part of teams and projects that are not just technology-driven but also vision-oriented, creating a future where technology acts as a catalyst for positive change and unprecedented innovation.",
-						]}
-						wrapper="p"
-						speed={75}
-						style={{
-							fontSize: "15px",
-							display: "inline-block",
-							fontFamily: "monospace",
-						}}
-						repeat={0}
-						deletionSpeed={90}
-					/>
-				</div>
-				<h3 id="workHeading">My Work</h3>
-				<hr />
-				<div id="workContainer">
-					{/* AI Learning Portal */}
-					<div className="workItemCard">
-						<h4 className="itemName">AI Learning Portal</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
-							className="container-work"
-							id="ai-learning-portal"
-						>
-							{" "}
-						</a>
+					<div id="workContainer">
+						{/* AI Learning Portal */}
+						<div className="workItemCard">
+							<h4 className="itemName">AI Learning Portal</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+								className="container-work"
+								id="ai-learning-portal"
+							>
+								{" "}
+							</a>
+						</div>
+						{/* NoSQL: Social Network API */}
+						<div className="workItemCard">
+							<h4 className="itemName">Social Network API</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/Jackspence6/NoSQL-social-network-api"
+								className="container-work"
+								id="social-network-api"
+							>
+								{" "}
+							</a>
+						</div>
+						{/* Tech Blog: A Developer's Tech Blog Platform */}
+						<div className="workItemCard">
+							<h4 className="itemName">Tech Blog</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/Jackspence6/tech-blog/tree/main"
+								className="container-work"
+								id="tech-blog"
+							>
+								{" "}
+							</a>
+						</div>
+						{/* Weather Dashboard */}
+						<div className="workItemCard">
+							<h4 className="itemName">Weather Dashboard</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://jackspence6.github.io/weather-dashboard/"
+								className="container-work"
+								id="weather-dashboard"
+							>
+								{" "}
+							</a>
+						</div>
+						{/* E-Commerce Back End */}
+						<div className="workItemCard">
+							<h4 className="itemName">E-Commerce Back End</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/Jackspence6/e-commerce-back-end-ORM-"
+								className="container-work"
+								id="eCommerce-back-end"
+							>
+								{" "}
+							</a>
+						</div>
+						{/* SQL: EmployeeTracker */}
+						<div className="workItemCard">
+							<h4 className="itemName">EmployeeTracker</h4>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://github.com/Jackspence6/EmployeeTrackerSQL"
+								className="container-work"
+								id="employee-tracker"
+							>
+								{" "}
+							</a>
+						</div>
 					</div>
-					{/* NoSQL: Social Network API */}
-					<div className="workItemCard">
-						<h4 className="itemName">Social Network API</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://github.com/Jackspence6/NoSQL-social-network-api"
-							className="container-work"
-							id="social-network-api"
-						>
-							{" "}
-						</a>
-					</div>
-					{/* Tech Blog: A Developer's Tech Blog Platform */}
-					<div className="workItemCard">
-						<h4 className="itemName">Tech Blog</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://github.com/Jackspence6/tech-blog/tree/main"
-							className="container-work"
-							id="tech-blog"
-						>
-							{" "}
-						</a>
-					</div>
-					{/* Weather Dashboard */}
-					<div className="workItemCard">
-						<h4 className="itemName">Weather Dashboard</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://jackspence6.github.io/weather-dashboard/"
-							className="container-work"
-							id="weather-dashboard"
-						>
-							{" "}
-						</a>
-					</div>
-					{/* E-Commerce Back End */}
-					<div className="workItemCard">
-						<h4 className="itemName">E-Commerce Back End</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://github.com/Jackspence6/e-commerce-back-end-ORM-"
-							className="container-work"
-							id="eCommerce-back-end"
-						>
-							{" "}
-						</a>
-					</div>
-					{/* SQL: EmployeeTracker */}
-					<div className="workItemCard">
-						<h4 className="itemName">EmployeeTracker</h4>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							href="https://github.com/Jackspence6/EmployeeTrackerSQL"
-							className="container-work"
-							id="employee-tracker"
-						>
-							{" "}
-						</a>
-					</div>
+					<hr />
 				</div>
 			</div>
+			<Footer />
 		</div>
 	);
 }
