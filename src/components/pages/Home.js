2ae4c22dfb57fd6e91304f6f126457c1712e72fd
Jack Spence <jackspence123456@gmail.com>
2023-12-14 12:19:10 +0200

    Added additional work items to portfolio.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 // Imports
 import React from "react";
-import "../../styles/Home.css";
 // Icons for Homepage
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 // Typing animation
 import { TypeAnimation } from "react-type-animation";
+import "../../styles/Home.css";
 
 // Exports
 export default function Home() {
@@ -37,11 +37,11 @@ export default function Home() {
 				</ul>
 			</div>
 			<div id="middleDiv">
-				<h1 id="name">Jack</h1>
-				<h2 id="surname">Spence</h2>
-				<h3 id="role">Full Stack Developer</h3>
-				<hr />
-				<p>
+				<div id="aboutContainer">
+					<h1 id="name">Jack</h1>
+					<h2 id="surname">Spence</h2>
+					<h3 id="role">Full Stack Developer</h3>
+					<hr />
 					<TypeAnimation
 						sequence={[
 							"Hi, my name is Jack Spence - A Full-Stack Engineer with a Vision for the Future! Having successfully completed a rigorous Full-Stack Engineering boot camp, I am now poised to dive deeper into the realms of technology that fascinate me the most - generative AI and machine learning. My journey in software development, enriched by my background in marketing, has provided me with a unique perspective on how technology can be leveraged to not just meet but anticipate user needs. My experience has endowed me with a robust foundation in various programming languages and frameworks. I am now keen on focusing my skills towards the cutting-edge fields of generative AI and machine learning. I am particularly interested in exploring how these technologies can be used to create innovative solutions and push the boundaries of what's possible in tech. Looking ahead, my goal is to be at the forefront of the AI revolution, contributing to projects that utilize AI and ML to solve complex problems, enhance user experiences, and open up new possibilities. I aspire to be part of teams and projects that are not just technology-driven but also vision-oriented, creating a future where technology acts as a catalyst for positive change and unprecedented innovation.",
@@ -56,11 +56,78 @@ export default function Home() {
 						repeat={0}
 						deletionSpeed={90}
 					/>
-				</p>
+				</div>
 				<h3>My Work</h3>
+				<hr />
 				<div id="workContainer">
+					{/* AI Learning Portal */}
 					<div className="workItemCard">
 						<h4>AI Learning Portal</h4>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+							className="container-work"
+							id="ai-learning-portal"
+						>
+							{" "}
+						</a>
+					</div>
+					{/* NoSQL: Social Network API */}
+					<div className="workItemCard">
+						<h4>Social Network API</h4>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+							className="container-work"
+							id="ai-learning-portal"
+						>
+							{" "}
+						</a>
+					</div>
+					{/* Tech Blog: A Developer's Tech Blog Platform */}
+					<div className="workItemCard">
+						<h4>Tech Blog</h4>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+							className="container-work"
+							id="ai-learning-portal"
+						>
+							{" "}
+						</a>
+					</div>
+					{/* Weather Dashboard */}
+					<div className="workItemCard">
+						<h4>Weather Dashboard</h4>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+							className="container-work"
+							id="ai-learning-portal"
+						>
+							{" "}
+						</a>
+					</div>
+					{/* E-Commerce Back End */}
+					<div className="workItemCard">
+						<h4>E-Commerce Back End</h4>
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://ai-learning-portal-c3740c057c06.herokuapp.com/"
+							className="container-work"
+							id="ai-learning-portal"
+						>
+							{" "}
+						</a>
+					</div>
+					{/* SQL: EmployeeTracker */}
+					<div className="workItemCard">
+						<h4>EmployeeTracker</h4>
 						<a
 							target="_blank"
 							rel="noreferrer"
